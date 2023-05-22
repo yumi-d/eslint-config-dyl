1610ad64333b159619344224ac5821c423258e91
dengyl <dengyl@hrtps.com>
2023-05-22 17:47:16 +0800

    feat: 添加.eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: dengyl@hrtps.com
+ * @Date: 2023-03-31 15:47:02
+ * @LastEditors: dengyl dengyl@hrtps.com
+ * @LastEditTime: 2023-03-31 18:33:47
+ * @FilePath: /theseus-admin/.eslintrc.js
+ */
+module.exports = {
+  extends: ['dyl'],
+  rules: {
+    'no-plusplus': 1,
+    'no-continue': 1,
+  },
+};
--- index.js
+++ index.js
@@ -2,38 +2,38 @@
  * @Author: dengyl@hrtps.com
  * @Date: 2023-03-31 15:47:02
  * @LastEditors: dengyl dengyl@hrtps.com
- * @LastEditTime: 2023-03-31 16:07:19
+ * @LastEditTime: 2023-05-22 17:45:56
  * @FilePath: /eslint-config-dyl/index.js
  */
+
 module.exports = {
-    extends: [
-      'airbnb',
-      'airbnb/hooks',
-    ],
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx'],
-        parser: '@typescript-eslint/parser',
-        plugins: ['@typescript-eslint'],
-        extends: ['plugin:@typescript-eslint/recommended'],
-        rules: {},
-      },
-    ],
-    rules: {
-        'import/extensions': 0,
-        'import/no-unresolved': 0,
-        'react/jsx-runtime': 0,
-        'react/jsx-filename-extension': 0,
-        'react/jsx-no-literals': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-max-depth': 0,
-        'arrow-body-style': 0,
-        'react-hooks/exhaustive-deps': 0,
-        'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-        'react/require-default-props': 0,
-        'no-unused-expressions': [1, {
-          allowShortCircuit: true,
-        }],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {},
     },
-  };
-  
+  ],
+  rules: {
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-runtime': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-literals': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-max-depth': 0,
+    'arrow-body-style': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/require-default-props': 0,
+    'no-unused-expressions': [1, {
+      allowShortCircuit: true,
+    }],
+  },
+};
